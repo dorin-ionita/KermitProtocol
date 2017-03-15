bdf6ccbb7d69d889db07b44f7a845121094f6fa5
Dorin <dorin.marian.ionita@gmail.com>
2017-03-15 21:34:31 +0200

    v4 - completely implemented sender

--- ksender.c
+++ ksender.c
@@ -39,32 +39,39 @@ void create_S_package(msg* t){
     t->payload[17] = '\0';
     t->len = strlen(t->payload);
 }
-void create_F_package(msg *t, char* file_name, int current_SEQ){
+// CHECK: pare ok
+
+void create_F_package(msg *t, char* file_name, char current_SEQ){
     t->payload[0] = SOH;        //SOH, mereu 0x01
     t->payload[1] = 5 + strlen(file_name);       //5 + lungime camp DATA
     t->payload[2] = current_SEQ;        //SEQ
     t->payload[3] = 'F';         //TYPE
     memcpy(&(t->payload[4]), file_name, strlen(file_name));
-    memcpy(&(t->payload[4 + strlen(file_name) + 1]),
-        &(crc16_ccitt(t->payload, 4 + strlen(file_name) + 1)), 2);
-    t->payload[4 + strlen(file_name) + 1 + 3] = EOL;
-    t->payload[4 + strlen(file_name) + 1 + 4] = '\0';
+    // ex: pt strlen = 1 avem ca la payload[4] se gaseste sirul,
+    // iar la payload[5 (si 6)] se gaseste crc
+    memcpy(&(t->payload[4 + strlen(file_name)]),
+        &(crc16_ccitt(t->payload, 4 + strlen(file_name))), 2);
+    t->payload[4 + strlen(file_name) + 2] = EOL; // 4 +1 +2 = 7 :D
+    t->payload[4 + strlen(file_name) + 3] = '\0';
     t->len = strlen(t->payload);
 }
+// CHECK: pare ok
 
-void create_D_package(msg *t, void* buffer_zone, int buffer_length, int current_SEQ){
+void create_D_package(msg *t, void* buffer_zone, int buffer_length, char current_SEQ){
     t->payload[0] = SOH;
     t->payload[1] = 5 + buffer_length;
     t->payload[2] = current_SEQ;
     t->payload[3] = 'D';
     memcpy((&(t->payload[4])), buffer_zone, buffer_length);
-    memcpy((&(t->payload[4] + buffer_length)), &(crc16_ccitt(t->payload, 4 + buffer_length)), 2);
+    memcpy((&(t->payload[4 + buffer_length])), &(crc16_ccitt(t->payload, 4 + buffer_length)), 2);
     t->payload[4 + payload[4] + buffer_length + 2] = EOL;
     t->payload[4 + payload[4] + buffer_length + 3] = '\0';
     t->len = strlen(t->payload);
 }
+// CHECK: pare ok
 
-void create_Z_package(msg *t, int current_SEQ){
+
+void create_Z_package(msg *t, char current_SEQ){
     t->payload[0] = SOH;
     t->payload[1] = 5;
     t->payload[2] = current_SEQ;
@@ -75,8 +82,9 @@ void create_Z_package(msg *t, int current_SEQ){
     t->payload[7] = '\0';
     t->len = strlen(t->payload);
 }
+// CHECK: pare ok
 
-void create_B_package(msg *t, int current_SEQ){
+void create_B_package(msg *t, char current_SEQ){
     t->payload[0] = SOH;
     t->payload[1] = 5;
     t->payload[2] = current_SEQ;
@@ -87,31 +95,38 @@ void create_B_package(msg *t, int current_SEQ){
     t->payload[7] = '\0';
     t->len = strlen(t->payload);
 }
+// CHECK: pare ok
 
 typedef struct {
     char npad;
     char padc;
     char eol;
 } ReceiverInfo;
+// CHECK: pare ok
 
 ReceiverInfo receiver;
+
+int is_acknowledgement(msg* answer){
+    return (answer->payload[3] == 'Y');
+}
 
 void get_receiver_info_from_ack(msg* answer){
     char npad = answer[6];
     char padc = answer[7];
     char eol = answer[8];
 }
+// CHECK: pare ok
 
-int send_name(char argv[], int current_SEQ){
+int send_name(char argv[], char current_SEQ){
     msg t;
     msg *answer;
     int i;
-    create_F_package(&t, argv, current_SEQ);
+    create_F_package(&t, argv, char current_SEQ);
     do{
         if (send_message(&t) < 0)
             return 1;
         for (i = 0 ; i < 3 ; i++){
-            if (answer = receive_message_timeout(5), answer)
+            if (answer = receive_message_timeout(TIME), answer)
                 break; // e garantat ca de la receptor vin doar date corecte
             if (i == 3)
                 return 1; // Termina conexiunea
@@ -120,9 +135,10 @@ int send_name(char argv[], int current_SEQ){
     return 0;
 }
 
-void send_file_with_name(char argv[], int current_SEQ){
+int send_file_with_name(char argv[], char *current_SEQ){
     send_name(argv, current_SEQ) ? return 1 : ;;
-    current_SEQ++;
+    (*current_SEQ)++;
+    (*current_SEQ) %= 64;
     int i;
     int file_handler = open(argv, O_RDONLY); // practic am deschis fisierul pentru citire
     int no_of_bytes_read = -1;
@@ -132,61 +148,76 @@ void send_file_with_name(char argv[], int current_SEQ){
     while (no_of_bytes_read){
         no_of_bytes_read = read (file_handler, buffer, MAXL);
         if (no_of_bytes_read) {
-            create_D_package(&t, buffer_zone, MAXL, current_SEQ);
+            create_D_package(&t, buffer_zone, MAXL, *current_SEQ);
             do{
                 if (send_message(&t) < 0)
                     return 1;
                 for (i = 0 ; i < 3 ; i++){
-                    if (answer = receive_message_timeout(5), answer)
+                    if (answer = receive_message_timeout(TIME), answer)
                         break; // e garantat ca de la receptor vin doar date corecte
-                if (i == 3)
-                    return 1; // Termina conexiunea
+                    if (i == 3)
+                        return 1; // Termina conexiunea
+                }
             } while (!is_acknowledgement(answer));
+            (*current_SEQ)++;
+            (*current_SEQ) %= 64;
         }
-        current_SEQ++;
     }
-    
+    create_Z_package(&t, *current_SEQ);
+    do{
+        if (send_message(&t) < 0)
+            return 1;
+        for (i = 0 ; i < 3 ; i++){
+            if (answer = receive_message_timeout(TIME), answer)
+                break; // e garantat ca de la receptor vin doar date corecte
+            if (i == 3)
+                return 1; // Termina conexiunea
+        }
+    } while (!is_acknowledgement(answer));
+    (*current_SEQ)++;
+    (*current_SEQ) %= 64;
     close(file_handler);
 }
 
 int main(int argc, char** argv) {
     // argc = nr de argumente + 1 (pentru numele executabilului)
+    int i;
     msg t;
     msg *answer;
     init(HOST, PORT);
-    // vrajeli ^
-
-/*    sprintf(t.payload, "Hello World of PC");
-    t.len = strlen(t.payload);
-    send_message(&t);*/
-
-    // send files
+    char current_SEQ = 0; // plec cu nr de secventa initial 0
     msg t;
     create_S_package(t);
     t.len = strlen(t.payload);
-    send_message(&t);
-    // TODO asteapta ACK
-    int i;
-    for (i = 0 ; i < 3 ; i++){
-        if (answer = receive_message_timeout(5), answer)
-            break; // e garantat ca de la receptor vin doar date corecte
-        if (i == 3)
-            return 1; // Termina conexiunea
-    }
+    do{
+        if (send_message(&t) < 0)
+            return 1;
+        for (i = 0 ; i < 3 ; i++){
+            if (answer = receive_message_timeout(TIME), answer)
+                break; // e garantat ca de la receptor vin doar date corecte
+            if (i == 3)
+                return 1; // Termina conexiunea
+        }
+    } while (!is_acknowledgement(answer));
+    current_SEQ++;
+    current_SEQ %= 64;
     get_receiver_info_from_ack(answer);
     for (i = 1 ; i < argc ; i++){
-        if (send_file_with_name(argv[i]))
+        if (send_file_with_name(argv[i], &current_SEQ))
             return 1; // daca intampin eroare, intorc eroare
     }
-
-
-    msg *y = receive_message_timeout(5000);
-    if (y == NULL) {
-        perror("receive error");
-    } else {
-        printf("[%s] Got reply with payload: %s\n", argv[0], y->payload);
-    }
-
-    
+    create_B_package(&t, current_SEQ);
+    do{
+        if (send_message(&t) < 0)
+            return 1;
+        for (i = 0 ; i < 3 ; i++){
+            if (answer = receive_message_timeout(TIME), answer)
+                break; // e garantat ca de la receptor vin doar date corecte
+            if (i == 3)
+                return 1; // Termina conexiunea
+        }
+    } while (!is_acknowledgement(answer));
+    current_SEQ++;
+    current_SEQ %= 64;
     return 0;
 }
